0a9f45931907d0d7fc47fa32e55033cda7ed9641
Sirawit Butmaratthaya <sirawitbm@gmail.com>
2024-10-31 16:33:58 +0700

    change welcome message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    model = {"title": "Hello DevOps."}
+    model = {"title": "Hello Bright."}
     return render_template('index.html', model=model)
 
 if __name__ == "__main__":
